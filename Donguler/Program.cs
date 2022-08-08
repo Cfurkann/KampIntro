e41a1194c0d6ba0073738d545034c22efaed5768
pc <pc@PC-BILGISAYAR>
2022-08-08 18:14:36 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kursu";
+            string kurs2 = "Programalamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+
+            //array
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kursu", "Programalamaya başlangıç için temel kurs","Java","Python","C#" };
+
+            for (int i = 0; i<kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);                                               
+            }
+
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu - footer");
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayısı = 32000;
+            double faizOranı = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış oku");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi oku");
+            }
+
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+       
+        }
+    }
+}
